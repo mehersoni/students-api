a1aedcf8fb69d1abfa566f6653a6eca272c901d1
mehersoni <mehersoni06@gmail.com>
2026-06-06 16:34:13 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import csv
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"]
+)
+
+students = []
+
+with open(r"C:\Users\Meher\students-api\q-fastapi.csv", newline="", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        students.append({
+            "studentId": int(row["studentId"]),
+            "class": row["class"]
+        })
+
+@app.get("/api")
+def get_students(class_: list[str] | None = Query(default=None, alias="class")):
+    if not class_:
+        return {"students": students}
+
+    filtered = [s for s in students if s["class"] in class_]
+    return {"students": filtered}
