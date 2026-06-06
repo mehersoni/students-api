9cab0a01baba996d88146fe1a26e87b3c6e92acc
mehersoni <mehersoni06@gmail.com>
2026-06-06 17:26:13 +0530

    add sentiment endpoint

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
 from typing import List
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class SentimentRequest(BaseModel):
     sentences: List[str]
 
